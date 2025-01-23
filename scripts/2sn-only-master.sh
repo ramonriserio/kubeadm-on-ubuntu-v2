003b4dcf13887a63fe6bdea85b256acd49c1f7ff
Ramon Risério <127441067+ramonriserio@users.noreply.github.com>
2025-01-23 12:28:12 -0300

    Update and rename only-master.sh to 2sn-only-master.sh

--- scripts/2sn-only-master.sh
+++ scripts/2sn-only-master.sh
@@ -1,21 +1,24 @@
 
 #!/bin/bash
 #
-# Setup for Control Plane (Master) servers
+# Após executar o script "1st-run-all-nodes.sh" (a ser executado em todos os nós, inclusive o master),
+# execute esse script somente no nó Master para congirar o Control Plane
 
+# Execute esse script como root user
+
+# O próximo comando garante que os pipelines falhem se qualquer comando dentro deles falhar
+# e trata váriaveis não definidas como erros
 set -euxo pipefail
 
-# If you need public access to API server using the servers Public IP adress, change PUBLIC_IP_ACCESS to true.
-
+# Se você precisa de acesso público ao API server usando IP público dos servidores, mude PUBLIC_IP_ACCESS para "true".
 PUBLIC_IP_ACCESS="false"
 NODENAME=$(hostname -s)
 POD_CIDR="172.168.0.0/16"
 
-# Pull required images
-
+# Faz o download das imagens requeridas
 sudo kubeadm config images pull
 
-# Initialize kubeadm based on PUBLIC_IP_ACCESS
+# Inicialize o kubeadm baseado no PUBLIC_IP_ACCESS
 
 if [[ "$PUBLIC_IP_ACCESS" == "false" ]]; then
     
@@ -32,12 +35,10 @@ else
     exit 1
 fi
 
-# Configure kubeconfig
-
+# Crie e configure o kubeconfig
 mkdir -p "$HOME"/.kube
 sudo cp -i /etc/kubernetes/admin.conf "$HOME"/.kube/config
 sudo chown "$(id -u)":"$(id -g)" "$HOME"/.kube/config
 
-# Install Claico Network Plugin Network 
-
+# Instale o plugin de rede Calico Network
 kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
